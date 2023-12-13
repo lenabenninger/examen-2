3423bfea8cf22607ef14cc976611f4d178cc6e9e
lenabenninger <lena.benninger@studentfr.ch>
2023-12-13 11:05:09 +0100

    transition burger menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 var burger = document.querySelector(".burger-menu");
 var menu = document.querySelector(".navigation-burger");
+var links = document.querySelector(".navigation-burger ul");
 var toggleMenu = function toggleMenu() {
   menu.classList.toggle("is-active");
+  burger.classList.toggle("is-active");
 };
 burger.addEventListener("click", toggleMenu);
+links.addEventListener("click", toggleMenu);
